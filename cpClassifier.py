7e8730b74304e1310c57b85673bf357e7b6e4ee5
nicoloRHUL <33029564+nicoloRHUL@users.noreply.github.com>
2024-01-26 17:27:28 +0000

    simplified version
    implementation of the four 2-parameter models described in the draft

--- cpClassifier.py
+++ cpClassifier.py
@@ -1,32 +1,49 @@
 import numpy
+import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.datasets import load_digits
+import matplotlib.colors as mcolors
 from scipy.optimize import minimize
 
 class transformScores:
-    def __init__(self, prob, X, y, obj, rho = 0):
-        self.n, self.K = prob.shape
+    def __init__(self, classifier, model, X, y, obj, rho = 0):
+        self.classifier = classifier
+        self.n, self.K = self.classifier.predict_proba(X).shape
         self.rho = rho 
         self.name = obj
-        self.rg = self.approximator(prob, X, y)
-        if obj == 'ML': 
-            self.obj = self.ML
-            self.theta = self.fitTransformation(prob, X, y)
-        
-        if obj == 'size': 
-            self.obj = self.smoothSize
-            self.theta = self.fitTransformation(prob, X, y)
-
-        if obj == 'unweighted': 
+        self.rg = self.approximator(X, y)
+        self.npars = 2 
+        self.model = model
+        if model == 'ER':
+            self.b = self.bER
+        if model == 'HR':
+            self.b = self.bHR
+        if model == 'temp':
+            self.b = self.bTemp
+        if model == 'off':
+            self.b = self.bOff
+        if model == 'unweighted':
+            self.b = self.bBase
             self.obj = None
-            self.theta = numpy.zeros(10)    
+            self.theta = numpy.zeros(self.npars) 
+            print(self.name,  ': done')
+            print('status, nit, theta: ', None, None, self.theta)
 
-    def basicScore(self, prob):
-        return 1 - prob
+        
+        if obj == 'ML' and model != 'unweighted': 
+            self.obj = self.ML
+            self.theta = self.fitTransformation(X, y)
+        
+        if obj == 'size' and model != 'unweighted': 
+            self.obj = self.smoothSize
+            self.theta = self.fitTransformation(X, y)
 
-    def approximator(self, prob, X, y):
-        A = self.basicScore(prob)
+    def basicScore(self, p, eps = 1e-2):
+        return 1 - p 
+
+    def approximator(self, X, y):
+        A = self.basicScore(self.classifier.predict_proba(X)) 
         a = [A[i, y[i]] for i in range(self.n)]
         rf = []
         for m in range(self.K): 
@@ -36,30 +53,41 @@ class transformScores:
             rf[-1].fit(xm, am.squeeze())
         return rf    
     
-    def prepareData(self, prob, X):
-        A = self.basicScore(prob)
-        G = numpy.array([self.rg[i].predict(X) for i in range(self.K)]).transpose()
-        h = numpy.array([H(1 - G[i, :]).squeeze() for i in range(self.n)])
-        return X, A, G, h
-
-    def r(self, t, G, h):
-        h = numpy.expand_dims(h, axis=1)
-        r1, r2 = [t[0 + i] + 
-                t[2 + i] * numpy.power(abs(G), t[4 + i]) + 
-                t[6 + i] * numpy.power(abs(h), t[8+ i]) for i in [0, 1]]
-        return r1, r2
+    def prepareData(self, p, x):
+        A = self.basicScore(p)
+        G = numpy.array([self.rg[i].predict(x) for i in range(self.K)]).transpose()
+        h = numpy.array([H(1 - G[i, :]).squeeze() for i in range(len(p))])
+        return x, A, G, h
+   
+    def bER(self, A, G, h, theta):
+        r = theta[0]**2 + theta[1]**2 * G
+        return A * numpy.exp(-r)
     
-    def b(self, A, G, h, theta):
-        r1, r2 = self.r(theta, G, h)
-        return A * numpy.exp(-r1) - r2 
+    def bBase(self, A, G, h, theta):
+        return A
+    
+    def bHR(self, A, G, h, theta):
+        H = numpy.expand_dims(h, axis=1)
+        r = theta[0]**2 + theta[1]**2 * H
+        return A/(1e-4 + r)
+    
+    def bTemp(self, A, G, h, theta):
+        r1 = theta[0]**2 * A#1 - rescale(1 - A, theta[0]**2)
+        r2 = 1 - rescale(1 - G, theta[1]**2)
+        return r1 + r2
+    
+    def bOff(self, A, G, h, theta):
+        r1 = A + theta[1] * G
+        r2 = sum(theta**2)
+        return r1/r2
 
-
-    def fitTransformation(self, prob, X, y):
-        d1 = self.prepareData(prob, X), y
-        initial_guess =  .1 * numpy.random.randn(10)
+    def fitTransformation(self, X, y):
+        d1 = self.prepareData(self.classifier.predict_proba(X), X), y
+        initial_guess =  .3 * numpy.random.randn(self.npars)
         optimal = minimize(self.obj, initial_guess,
-                args=(d1, d1), options={'maxiter': 1000}, tol = 1e-3)
-        print(self.name,  ': done')
+                args=(d1, d1),# method='CG', 
+                options={'maxiter': 100}, tol = 1e-4)
+        print(self.name, self.model,  ': done')
         print('status, nit, theta: ', optimal.success, optimal.nit, optimal.x)
         return optimal.x
  
@@ -67,52 +95,62 @@ class transformScores:
         [X1, A1, G1, h1], y1 = d1
         [X2, A2, G2, h2], y2 = d2
         B1 = self.b(A1, G1, h1, theta)
-        b1 = numpy.array([B1[i, y1[i]] for i in range(self.n)])
+        b1 = numpy.array([B1[i, y1[i]] for i in range(len(y1))])
         b1 = numpy.expand_dims(b1, axis = 1)
         B2 = self.b(A2, G2, h2, theta)
-        b2 = numpy.array([B2[i, y2[i]] for i in range(self.n)])
+        b2 = numpy.array([B2[i, y2[i]] for i in range(len(y1))])
         b2 = numpy.expand_dims(b2, axis = 1)
         M = b1 - b2.transpose()
         m = numpy.linalg.norm(M - numpy.diag(numpy.diag(M))) 
-        ell = m/self.n
+        ell = m/len(y1)
         return ell + self.rho * theta @ theta
 
-    def smoothSize(self, theta, d1, d2, scale = 2):
-        [X1, A1, G1, h1], y1 = d1
-        [X2, A2, G2, h2], y2 = d2
-        beta = 2
-        B1 = self.b(A1, G1, h1, theta)
+    def smoothSize(self, theta, d1, d2, scale = 1):
+        z1, y1 = d1
+        #[X2, A2, G2, h2]
+        z2, y2 = d2
+        half = int(len(y2)/2)
+        z1, y1 = [x[:half] for x in z1], y1[:half]
+        z2, y2 = [x[half:] for x in z2], y2[half:]
+        X1, A1, G1, h1 = z1
+        X2, A2, G2, h2 = z2
         B2 = self.b(A2, G2, h2, theta)
-        b2 = numpy.array([B2[i2, y2[i2]] for i2 in range(self.n)])
-        w = b2/sum(b2)
-        w = numpy.exp(beta * w) + 1e-4
-        w = numpy.diag(w / sum(w))
+        b2 = numpy.array([B2[i2, y2[i2]] for i2 in range(len(y2))])
+        beta = 1
+        w = numpy.diag(rescale(
+            numpy.expand_dims(b2, axis=1).transpose(), 
+            beta).squeeze())
+        B1 = self.b(A1, G1, h1, theta)
         B1 = numpy.expand_dims(B1, axis = 2)
         B1 = numpy.transpose(B1, axes = [0, 2, 1])
         b2 = numpy.array(b2)
         b2 = numpy.expand_dims(b2, axis = [0, 2])
-        S = numpy.sum(sigma(scale * (-(B1 - b2))), axis = 2)
-        ell = 1/self.n * numpy.sum(S @ w)
+        S = numpy.sum(sigma(- scale *(B1 - b2)/numpy.linalg.norm(B1)), 
+            axis = 2)
+        ell = 1/len(y1) * numpy.sum(S @ w)
         return ell + self.rho * theta @ theta
 
-    def predict(self, prob, X):
-        X, A, G, h = self.prepareData(prob, X)
+    def predict(self, x):
+        p = self.classifier.predict_proba(x) 
+        x, A, G, h = self.prepareData(p, x)
         return self.b(A, G, h, self.theta)
     
-    def evaluateCPclass(self, d1, d2, classifier, alpha = .1):
+    def evaluateCPclass(self, d1, d2, alpha = .1):
         X, y = d1
-        X, A, G, h = self.prepareData(classifier.predict_proba(X), X)
+        print(len(X))
+        X, A, G, h = self.prepareData(self.classifier.predict_proba(X), X)
         B = self.b(A, G, h, self.theta)
         q = quantile([B[i, y[i]] for i in range(len(y))], alpha)
         
         X, y = d2
-        X, A, G, h = self.prepareData(classifier.predict_proba(X), X)
+        X, A, G, h = self.prepareData(self.classifier.predict_proba(X), X)
         B = self.b(A, G, h, self.theta)
         sets = [[m for m in range(self.K) if B[i][m] < q] for i in range(len(y))]
-        F1 = F1score(sets, y)
+        F1s, F1cps= F1score(sets, y)
+        corrs = HScorr(1 - A, sets)
         sizes = numpy.sum([len(sets[i]) for i in range(len(sets))])/len(y)
         val = numpy.sum([1 for i in range(len(y)) if y[i] in sets[i]])/len(y)
-        return sizes, F1, val
+        return val, sizes, F1s, F1cps, corrs
 
 def classifier(D):
     X, Y = D
@@ -127,6 +165,10 @@ def quantile(v, alpha):
     m = int(numpy.ceil((1 - alpha) * (len(v) + 1))) - 1
     v = numpy.sort(v, axis = 0)
     return v[m]
+
+def rescale(V, beta):
+    S = numpy.exp(beta * V)
+    return  S/numpy.expand_dims(numpy.sum(S, axis=1), axis=1) 
 
 def H(p, eps=.0001):
     return -sum([s * numpy.log(s + eps) for s in p])
@@ -143,7 +185,29 @@ def F1score(sets, y):
         TN = TN + sum([1 for j in negatives if j != y[i]])  
         FN = FN + sum([1 for j in negatives if j == y[i]]) 
     F1 = 2 * TP/(2 * TP + FP + FN)
-    return F1
+    F1cp = F1CPscore(sets, y)#F2 = (TP + TN)/(FP + FN)
+    return F1, F1cp
+
+def HScorr(F, sets):
+    sizes = numpy.array([len(x) for x in sets])
+    h = numpy.array([H(f) for f in F])
+    return (sizes@h)/numpy.sqrt(1e-4 + (sizes@sizes) * (h @ h))
+
+def F1CPscore(sets, y):
+    mclasses = max(y) + 1
+    TP, FP, TN, FN = [[0 for m in range(mclasses)] for k in [0, 1, 2, 3]]
+    for i in range(len(sets)):
+        interval = sets[i]
+        positives = [j for j in range(mclasses) if j in interval]
+        negatives = [j for j in range(mclasses) if j not in interval]
+        for m in range(mclasses):
+            TP[m] = TP[m] + 1 * (m in positives) * (y[i] == m)# in positives)
+            FP[m] = FP[m] + 1 * (m in positives) * (y[i] != m)# in negatives)
+            TN[m] = TN[m] + 1 * (m in negatives) * (y[i] !=m)# in negatives) 
+            FN[m] = FN[m] + 1 * (m in negatives) * (y[i] == m)# in positives) 
+    F1cp = numpy.prod([2 * TP[m]/(2 * TP[m] + FP[m] + FN[m]) for m in  range(mclasses)])
+    return F1cp
+
 
 def splitData(X, y):
     t = 3 #train, cal, test
@@ -152,15 +216,18 @@ def splitData(X, y):
 
 
 #load and random-split the data
-numpy.random.seed(12345)
+numpy.random.seed(1234)
 digits = load_digits()
 Xall = digits.images
 yall = digits.target
 Xall = Xall.reshape([len(Xall), 64])
 
 #run experiments
-results = [[] for i in [0, 1, 2]]
-for k in [0, 1, 2, 4, 5]:
+bNames = ['unweighted', 'ER', 'HR', 'temp', 'off']
+objs = ['ML', 'size']#['ML', 'size']#['size', 'ML', 'unweighted']
+rhos = [0.01, 0.01]#0.1, 0]#[0, .01, 0]
+results = []
+for k in range(3):
     alpha = .1
     choice = numpy.random.choice(len(Xall), size=len(Xall), replace=False)
     X, y = Xall[choice], yall[choice]
@@ -173,29 +240,56 @@ for k in [0, 1, 2, 4, 5]:
     dataset = train
     rfClass = classifier([dataset[0], dataset[1]])
     print(rfClass)
-    X, y = train
+    nSamples = 1000
+    X, y = [train[i][:nSamples] for i in [0, 1]]
     Xt, yt = test
     Xc, yc = cal
-    bModels = []
-    objs = ['ML', 'size', 'unweighted']#['size', 'ML', 'unweighted']
-    rhos = [0.1, 0, 0]#[0, .01, 0]
+    bModels = [[] for i in bNames]
     for iobj in range(len(objs)):
         obj = objs[iobj]
         rho = rhos[iobj]
-        bModels.append(
-                transformScores(rfClass.predict_proba(X), X, y, obj, rho))
+        for ib in range(len(bNames)):
+            bModels[ib].append(
+                transformScores(rfClass, bNames[ib], X, y, obj, rho))
     
-    for iB in range(len(bModels)):
-        model = bModels[iB]
-        size, f1, val = model.evaluateCPclass(cal, test, rfClass, alpha = .1)
-        print('size, f1, val', size, f1, val)
-        results[iB].append([size, f1, val])
+    r = [[] for i in bNames]
+    for iobjs in range(len(objs)):
+        for ib in range(len(bNames)): 
+            model = bModels[ib][iobjs]
+            score = model.evaluateCPclass(cal, test, alpha = .1)
+            val, size, f1, f1cp, corr = score
+            print(bNames[ib], objs[iobjs])
+            print(bModels[ib][iobjs].theta)
+            print('val, size, f1, f1cp, corr', score)
+            r[ib].append(score)
 
-results = numpy.array(results)
-means = numpy.mean(results/results[-1], axis=1)
-std = numpy.std(results/results[-1], axis = 1)
-
-print('average (5 runs) relative size, f1, validity [ml, smoothSize] ', means[:-1])
-print('std deviation (5 runs) size, f1, validity [ml, smoothSize]', std[:-1])
+    results.append(r)
 print(results)
+date='20240126'
+numpy.save('results'+date, results)
+results = numpy.load('results'+date+'.npy')
+nr = results/numpy.expand_dims(results[:, 0, :, :], axis=1)
+means=numpy.mean(nr, axis=0)
+stds=numpy.std(nr, axis=0)
+
+print('size')
+iScore = 1 
+for ib in range(1, len(bNames)):
+    print(bNames[ib])
+    print(means[ib,:, iScore])
+
+print('f1cp')
+iScore = 3 
+for ib in range(1, len(bNames)):
+    print(bNames[ib])
+    print(means[ib,:, iScore])
+
+print('corr')
+iScore = 4 
+for ib in range(1, len(bNames)):
+    print(bNames[ib])
+    print(means[ib,:, iScore])
+
+
+
 
